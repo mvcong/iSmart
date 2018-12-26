d5fc30f5d6d83a0bc01bce419de4542ae42b3235
mvcong <mai.cong365@gmail.com>
2018-12-26 15:48:03 +0700

    iSmart

--- src/iCore/model/GioHang.java
+++ src/iCore/model/GioHang.java
@@ -13,11 +13,10 @@ import sanpham.SanPham;
 public class GioHang {
 	@Id
 	public String maGioHang;
-	public SanPham tenSP;
-	public SanPham giaSP;
-	public String soLuong;
+	public String tenSP;
+	public long giaSP;
+	public int soLuong;
 	public long thanhTien;
-	public Date ngayMua;
 	public Date thoiGianCapNhat;
 
 	@ManyToOne(fetch = FetchType.EAGER)
@@ -27,14 +26,13 @@ public class GioHang {
 		// TODO Auto-generated constructor stub
 	}
 
-	public GioHang(String maGioHang, SanPham tenSP, SanPham giaSP, String soLuong, long thanhTien, Date ngayMua,
-			Date thoiGianCapNhat, ThanhVien thanhVien) {
+	public GioHang(String maGioHang, String tenSP, long giaSP, int soLuong, long thanhTien, Date thoiGianCapNhat,
+			ThanhVien thanhVien) {
 		this.maGioHang = maGioHang;
 		this.tenSP = tenSP;
 		this.giaSP = giaSP;
 		this.soLuong = soLuong;
 		this.thanhTien = thanhTien;
-		this.ngayMua = ngayMua;
 		this.thoiGianCapNhat = thoiGianCapNhat;
 		this.thanhVien = thanhVien;
 	}
@@ -47,27 +45,27 @@ public class GioHang {
 		this.maGioHang = maGioHang;
 	}
 
-	public SanPham getTenSP() {
+	public String getTenSP() {
 		return tenSP;
 	}
 
-	public void setTenSP(SanPham tenSP) {
+	public void setTenSP(String tenSP) {
 		this.tenSP = tenSP;
 	}
 
-	public SanPham getGiaSP() {
+	public long getGiaSP() {
 		return giaSP;
 	}
 
-	public void setGiaSP(SanPham giaSP) {
+	public void setGiaSP(long giaSP) {
 		this.giaSP = giaSP;
 	}
 
-	public String getSoLuong() {
+	public int getSoLuong() {
 		return soLuong;
 	}
 
-	public void setSoLuong(String soLuong) {
+	public void setSoLuong(int soLuong) {
 		this.soLuong = soLuong;
 	}
 
@@ -77,14 +75,6 @@ public class GioHang {
 
 	public void setThanhTien(long thanhTien) {
 		this.thanhTien = thanhTien;
-	}
-
-	public Date getNgayMua() {
-		return ngayMua;
-	}
-
-	public void setNgayMua(Date ngayMua) {
-		this.ngayMua = ngayMua;
 	}
 
 	public Date getThoiGianCapNhat() {
@@ -107,10 +97,9 @@ public class GioHang {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((giaSP == null) ? 0 : giaSP.hashCode());
+		result = prime * result + (int) (giaSP ^ (giaSP >>> 32));
 		result = prime * result + ((maGioHang == null) ? 0 : maGioHang.hashCode());
-		result = prime * result + ((ngayMua == null) ? 0 : ngayMua.hashCode());
-		result = prime * result + ((soLuong == null) ? 0 : soLuong.hashCode());
+		result = prime * result + soLuong;
 		result = prime * result + ((tenSP == null) ? 0 : tenSP.hashCode());
 		result = prime * result + (int) (thanhTien ^ (thanhTien >>> 32));
 		result = prime * result + ((thanhVien == null) ? 0 : thanhVien.hashCode());
@@ -127,25 +116,14 @@ public class GioHang {
 		if (getClass() != obj.getClass())
 			return false;
 		GioHang other = (GioHang) obj;
-		if (giaSP == null) {
-			if (other.giaSP != null)
-				return false;
-		} else if (!giaSP.equals(other.giaSP))
+		if (giaSP != other.giaSP)
 			return false;
 		if (maGioHang == null) {
 			if (other.maGioHang != null)
 				return false;
 		} else if (!maGioHang.equals(other.maGioHang))
 			return false;
-		if (ngayMua == null) {
-			if (other.ngayMua != null)
-				return false;
-		} else if (!ngayMua.equals(other.ngayMua))
-			return false;
-		if (soLuong == null) {
-			if (other.soLuong != null)
-				return false;
-		} else if (!soLuong.equals(other.soLuong))
+		if (soLuong != other.soLuong)
 			return false;
 		if (tenSP == null) {
 			if (other.tenSP != null)
@@ -170,8 +148,8 @@ public class GioHang {
 	@Override
 	public String toString() {
 		return "GioHang [maGioHang=" + maGioHang + ", tenSP=" + tenSP + ", giaSP=" + giaSP + ", soLuong=" + soLuong
-				+ ", thanhTien=" + thanhTien + ", ngayMua=" + ngayMua + ", thoiGianCapNhat=" + thoiGianCapNhat
-				+ ", thanhVien=" + thanhVien + "]";
+				+ ", thanhTien=" + thanhTien + ", thoiGianCapNhat=" + thoiGianCapNhat + ", thanhVien=" + thanhVien
+				+ "]";
 	}
 
 }
